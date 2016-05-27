d900ed72c692a0de6e432ca42df731c5f9c22ba5
Ben Webb <ben@salilab.org>
2016-05-27 10:22:07 -0700

    Split long strings on mmCIF output.
    Long strings can be output in mmCIF by splitting them
    into multiple lines and using "\n;" to delimit the string value.

--- imp_pmi/__init__.py
+++ imp_pmi/__init__.py
@@ -7,11 +7,20 @@ import os
 import operator
 
 class _LineWriter(object):
-    def __init__(self, writer, line_len=80):
+    def __init__(self, writer, line_len=80, multi_line_len=70):
         self.writer = writer
         self.line_len = line_len
+        self.multi_line_len = multi_line_len
         self.column = 0
     def write(self, val):
+        if isinstance(val, str) and len(val) > self.multi_line_len:
+            self.writer.fh.write("\n;")
+            for i in range(0, len(val), self.multi_line_len):
+                self.writer.fh.write(val[i:i+self.multi_line_len])
+                self.writer.fh.write("\n")
+            self.writer.fh.write(";\n")
+            self.column = 0
+            return
         val = self.writer._repr(val)
         if self.column > 0:
             if self.column + len(val) + 1 > self.line_len:
